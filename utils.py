be6d675ddd59e6caec4f71dd7a400fd943925e7f
phutaekwondo <phutaekwondo2001@gmail.com>
2023-05-11 13:57:25 +0700

    face detect

---
+++ utils.py
@@ -0,0 +1,59 @@
+from mtcnn import MTCNN
+import tensorflow as tf
+import cv2
+
+# tf.debugging.set_log_device_placement(True)
+detector = MTCNN()
+opencv_face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+def get_face_bb_opencv(img):
+    """
+    Get the bounding box of the face in the image
+    :param img: image
+    :return: bounding box of the face
+    """
+    # detect faces in the image
+    faces = opencv_face_detector.detectMultiScale(img, 1.3, 5)
+
+    if len (faces) <= 0: return None
+    # extract the bounding box from the first face
+    x1, y1, width, height = faces[0]
+    x2, y2 = x1 + width, y1 + height
+    return x1, y1, x2, y2
+
+def get_face_bb_mtcnn(img):
+    """
+    Get the bounding box of the face in the image
+    :param img: image
+    :return: bounding box of the face
+    """
+    # detect faces in the image
+    faces = detector.detect_faces(img)
+
+    if len (faces) <= 0: return None
+    # extract the bounding box from the first face
+    x1, y1, width, height = faces[0]['box']
+    x2, y2 = x1 + width, y1 + height
+    return x1, y1, x2, y2
+
+def crop_face(img, bb):
+    """
+    Crop the face from the image
+    :param img: image
+    :param bb: bounding box
+    :return: cropped face
+    """
+    return img[bb[1]:bb[3], bb[0]:bb[2]]
+
+def draw_bb_on_img(img, bb, color):
+    """
+    Draw bounding box on the image
+    :param img: image
+    :param bb: bounding box
+    :return: image with bounding box
+    """
+    # draw the bounding box
+    cv2.rectangle(img, (bb[0], bb[1]), (bb[2], bb[3]), color, 2)
+    return img
+
+
