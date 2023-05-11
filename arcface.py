a036a6581ec3630f543658a3c262094229b46eac
chicuongdx <chicuongdx2001@gmail.com>
2023-05-11 14:34:32 +0700

    face recognition using arcface

---
+++ arcface.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+import numpy as np
+from deepface import DeepFace
+import math
+import cv2
+
+class ArcFaceModel:
+    def __init__(self, model_path = None):
+        if model_path == None:
+            self.model = DeepFace.build_model("ArcFace")
+        else:
+            self.model = tf.keras.models.load_model(model_path)
+        
+        self.threshold = 0.4
+        self.face_register = {}
+    
+    def register_face(self, name, image):
+        embedding = self.predict(image)
+        self.face_register[name] = embedding
+
+    def recognize_face(self, image):
+        embedding = self.predict(image)
+        for name in self.face_register.keys():
+            if self.match(embedding, self.face_register[name]):
+                return name
+        return None
+
+    def summary(self):
+        self.model.summary()
+
+    def predict(self, image):
+        image = cv2.resize(image, (112, 112))
+        image = np.expand_dims(image, axis=0)
+        image = image.astype(np.float32)
+        image = image / 255.0
+        embedding = self.model.predict(image)
+        return embedding
+
+    def match(self, embedding1, embedding2):
+        cosine_similarity = -1 * tf.keras.losses.cosine_similarity(embedding1, embedding2)
+        
+        try:
+            cosine_similarity = cosine_similarity.numpy()[0]
+        except:
+            cosine_similarity = 0
+        
+        print(cosine_similarity)
+
+        return cosine_similarity > self.threshold
+    
+    def cosine(self, embedding1, embedding2):
+       
+        cosine = -1 * tf.keras.losses.cosine_similarity(embedding1, embedding2)
+        
+        try:
+            cosine = cosine.numpy()[0]
+        except:
+            cosine = 0
+        
+        return cosine
+    
+    def set_threshold(self, threshold):
+        self.threshold = threshold
+        return self.threshold
+    
+    def find_good_threshold(self, list_image):
+
+        threshold = 0.5
+
+        # # Load LFW dataset
+        # import tensorflow_datasets as tfds
+        # lfw = tfds.load("lfw")
+
+        # # Calculate embeddings for LFW dataset
+        # lfw_embeddings = {}
+        # for name in lfw.keys():
+        #     lfw_embeddings[name] = []
+        #     for image in lfw[name]:
+        #         embedding = model.predict(cv2.resize(image, (112, 112)).astype(np.float32) / 255.0)
+        #         lfw_embeddings[name].append(embedding)
+
+        # Calculate embeddings for input images
+        input_embeddings = []
+        for image in list_image:
+            print(image.shape)
+            embedding = self.predict(image)
+            input_embeddings.append(embedding)
+
+        # Calculate cosine similarity between each pair of embeddings
+        similarities = []
+        for i in range(len(input_embeddings)):
+            for j in range(i+1, len(input_embeddings)):
+                similarity = self.cosine(input_embeddings[i], input_embeddings[j])
+                similarities.append(similarity)
+        
+        # Plot histogram of similarities
+        import matplotlib.pyplot as plt
+        plt.hist(similarities, bins=100)
+        plt.show()
+
+        # Choose threshold that separates positive and negative pairs well
+        threshold = np.mean(similarities) - np.std(similarities)
+        
+        return threshold
---
+++ face_recognition.py
@@ -0,0 +1,71 @@
+import utils
+import cv2
+import time
+from arcface import ArcFaceModel
+
+# initialize the camera
+cap = cv2.VideoCapture(0)
+face_cropped = None
+
+arcface = ArcFaceModel()
+
+face_cuong = cv2.imread('img/cuong.jpg')
+
+arcface.register_face('cuong', face_cuong)
+
+while True:
+    # with FPS
+    start_time = time.time()
+    # ret, frame = cap.read()
+    # if not ret: break
+    # frame = cv2.flip(frame, 1)
+    # frame = cv2.resize(frame, (640, 480))
+    # bb = utils.get_face_bb(frame)
+    # if bb is not None:
+    #     frame = utils.draw_bb_on_img(frame, bb, (0, 255, 0))
+    # cv2.putText(frame, "FPS: {:.2f}".format(1.0 / (time.time() - start_time)), (10, 30),
+    #             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+    # cv2.imshow("Frame", frame)
+    # if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+    # get the frame
+    _, frame = cap.read()
+
+    # get the bounding box
+    bb = utils.get_face_bb_opencv(frame)
+    # draw the bounding box on the frame
+    if bb is not None:
+        frame = utils.draw_bb_on_img(frame, bb, (0, 255, 0))
+        face_cropped = utils.crop_face(frame, bb)
+
+    # get frame height
+    height = frame.shape[0]
+
+    #resize face_cropped to height
+    if face_cropped is not None:
+        face_cropped = cv2.resize(face_cropped, (height, height))
+
+        # get the name of the person
+        name = arcface.recognize_face(face_cropped)
+
+        # draw the name of the person
+        cv2.putText(frame, name, (bb[0], bb[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+    #concat frame and face_cropped
+    frame = cv2.hconcat([frame, face_cropped])
+
+    #show the FPS
+    cv2.putText(frame, "FPS: {:.2f}".format(1.0 / (time.time() - start_time)), (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+    # show the frame
+    cv2.imshow("frame", frame)
+    # wait for key press
+    key = cv2.waitKey(1)
+    # if the key pressed is `q` then break from the loop
+    if key == ord("q"):
+        break
+
+# release the camera
+cap.release()
+# close all the frames
+cv2.destroyAllWindows()
