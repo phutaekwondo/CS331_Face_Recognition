d9c9fa203b93764c2da7c6fbd174673d85d402f1
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-05-12 08:24:37 +0700

    get biggest face bounding box with opencv method

--- utils.py
+++ utils.py
@@ -15,7 +15,23 @@ def mtcnn_init():
     global mtcnn_detector 
     mtcnn_detector = MTCNN() 
 
-def get_face_bb_opencv(img):
+def get_biggest_bb(bbs):
+    """
+    Get the biggest bounding box
+    :param bbs: bounding boxes
+    :return: biggest bounding box
+    """
+    if bbs is None: return None
+    biggest_bb = None
+    biggest_bb_area = 0
+    for bb in bbs:
+        area = (bb[2] - bb[0]) * (bb[3] - bb[1])
+        if area > biggest_bb_area:
+            biggest_bb_area = area
+            biggest_bb = bb
+    return biggest_bb
+
+def get_all_face_bbs_opencv(img):
     """
     Get the bounding box of the face in the image
     :param img: image
@@ -26,9 +42,23 @@ def get_face_bb_opencv(img):
 
     if len (faces) <= 0: return None
     # extract the bounding box from the first face
-    x1, y1, width, height = faces[0]
-    x2, y2 = x1 + width, y1 + height
-    return x1, y1, x2, y2
+    bbs = []
+    for face in faces:
+        x1, y1, width, height = face
+        x2, y2 = x1 + width, y1 + height
+        bbs.append((x1, y1, x2, y2))
+    return bbs
+
+def get_face_bb_opencv(img):
+    """
+    Get the bounding box of the face in the image
+    :param img: image
+    :return: bounding box of the face
+    """
+    # detect faces in the image
+    face_bbs = get_all_face_bbs_opencv(img)
+    return get_biggest_bb(face_bbs)
+
 
 def get_face_bb_mtcnn(img):
     """
